40881f64a561123fd996de446c57afd3152efa08
NicolayChillo <nicolaychillo13@gmail.com>
2026-05-12 12:56:58 -0500

    ExamenParcial1

---
+++ Primer Parcial/ExamenParcial/chillo_examen_parcial1/lib/tema/export.dart
@@ -0,0 +1,16 @@
+export 'package:tema_personalizado/tema/colores_app.dart';
+export 'package:tema_personalizado/tema/tipografia_app.dart';
+export 'package:tema_personalizado/tema/tema_general.dart';
+
+// COMPONENTES
+export 'package:tema_personalizado/tema/componentes/tema_appbar.dart';
+export 'package:tema_personalizado/tema/componentes/tema_botones.dart';
+export 'package:tema_personalizado/tema/componentes/tema_inputs.dart';
+export 'package:tema_personalizado/tema/componentes/tema_cards.dart';
+export 'package:tema_personalizado/tema/componentes/tema_checkbox.dart';
+export 'package:tema_personalizado/tema/componentes/tema_floating.dart';
+
+// DECORACIONES
+export 'package:tema_personalizado/tema/decoraciones/fondos_app.dart';
+export 'package:tema_personalizado/tema/decoraciones/bordes_app.dart';
+export 'package:tema_personalizado/tema/decoraciones/sombras_app.dart';
